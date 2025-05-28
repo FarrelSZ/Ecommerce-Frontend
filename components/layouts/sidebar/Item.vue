edfaf3e7f49f833a39e45901a8f9cd9357852432
FarrelSZ <farel.it12@gmail.com>
2025-05-28 21:22:21 +0700

    blm selesai

---
+++ components/layouts/sidebar/Item.vue
@@ -0,0 +1,29 @@
+<template>
+  <NuxtLink v-if="!item.children" :to="item.to">
+    <div class="flex gap-2 items-center hover:text-primary">
+      <UIcon v-if="item.icon" :name="item.icon" class="w-6 h-6" />
+      <p class="text-sm">{{ item.label }}</p>
+    </div>
+  </NuxtLink>
+  <template v-else>
+    <UAccordion
+      :items="[
+        {
+          label: item.label,
+          children: item.children,
+        },
+      ]"
+    ></UAccordion>
+  </template>
+</template>
+
+<script setup>
+defineProps({
+  item: {
+    type: Object,
+    default: () => ({}),
+  },
+});
+</script>
+
+<style scoped></style>
--- layouts/seller.vue
+++ layouts/seller.vue
@@ -1,11 +1,57 @@
 <template>
   <div class="seller-layout flex flex-col min-h-screen relative text-slate-800">
     <LayoutsHeaderSeller />
-    <!-- <aside>sidebar</aside> -->
+    <aside class="seller-sidebar bg-white p-4 fixed left-0 bottom-0">
+      <LayoutsSidebar :items="sidebarItems" />
+    </aside>
     <main class="main-content flex-1 bg-black/10"><slot /></main>
   </div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import type { AccordionItem } from "@nuxt/ui";
 
-<style scoped></style>
+const sidebarItems = ref<AccordionItem[]>([
+  {
+    label: "Pesanan",
+    icon: "i-heroicons:clipboard-document-list",
+    to: "/seller/order",
+  },
+  {
+    label: "Produk",
+    icon: "i-heroicons:clipboard-document-list",
+    children: [
+      {
+        label: "Produk Saya",
+        to: "/seller/product",
+      },
+      {
+        label: "Tambah Produk Baru",
+        to: "/seller/product/add",
+      },
+    ],
+  },
+  {
+    label: "Voucher",
+    icon: "i-heroicons:receipt-percent",
+    to: "/seller/voucher",
+  },
+  {
+    label: "Keuangan",
+    icon: "i-heroicons:banknotes",
+    children: [
+      {
+        label: "Saldo Saya",
+        to: "/seller/balance",
+      },
+    ],
+  },
+]);
+</script>
+
+<style scoped>
+.seller-sidebar {
+  top: 56px;
+  width: 240px;
+}
+</style>
