0b6db7ca5396a9a6c3c29d79695e826bffbd757b
FarrelSZ <farel.it12@gmail.com>
2025-05-28 21:49:11 +0700

    masih blm jadi

--- components/layouts/sidebar/Item.vue
+++ components/layouts/sidebar/Item.vue
@@ -13,7 +13,28 @@
           children: item.children,
         },
       ]"
-    ></UAccordion>
+    >
+      <template #default="{ open, item: parent }">
+        <div class="flex gap-2 items-center hover:text-primary" role="button">
+          <UIcon v-if="parent.icon" :name="parent.icon" class="w-6 h-6" />
+          <p class="text-sm">{{ parent.label }}</p>
+          <!-- <UIcon
+            name="i-heroicons:chevron-down-20-solid"
+            class="w-4 h-4 transition-all"
+            :class="[open && 'rotate-180']"
+          /> -->
+        </div>
+      </template>
+      <template #item>
+        <div class="pl-8 flex flex-col gap-2">
+          <LayoutsSidebarItem
+            v-for="(child, index) in item.children"
+            :key="`child-${child.label}-${index}`"
+            :item="child"
+          />
+        </div>
+      </template>
+    </UAccordion>
   </template>
 </template>
 
